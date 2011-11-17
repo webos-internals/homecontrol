19f774304d7a04dc30e04d22c431445d1541679f
Janne Julkunen <scorpio.iix@gmail.com>
2011-11-17 04:42:15 +0200

    Finished keyboard / mouse support

--- enyo-app/source/extensions/ComputerInput.js
+++ enyo-app/source/extensions/ComputerInput.js
@@ -9,6 +9,10 @@ enyo.kind({
 
 	_keyboard: false,
 	
+	_function: false,
+	
+	_shift: false,
+	
 	_waiting: false,
 	
 	events: {
@@ -22,6 +26,49 @@ enyo.kind({
 	},
 	
 	components: [
+		{name: "keysPopup", kind: "Popup", style: "max-width: 500px;", onclick: "handlePopupClose", onClose: "handlePopupClose", components: [
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "1..0, Q, W", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "F1..F10, F11, F12", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]},
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "B, J, N , M", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "Arrow Keys", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]},
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "Y", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "Home", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]},
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "H", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "End", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]},
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "I", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "Page Up", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]},
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "K", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "Page Down", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]},
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "U", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "Ins", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]},
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "Backspace", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "Del", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]},
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "Enter", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "Esc", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]},
+			{layoutKind: "HFlexLayout", components: [	
+		    	{content: "Space", flex: 1, style: "text-align: left;font-size: 16px;"},
+		    	{content: "Tab", flex: 1, style: "text-align: right;font-size: 16px;"}
+		   ]}
+		]},
+	
 		{kind: "PageHeader", layoutKind: "HFlexLayout", components: [
 			{name: "normalHeader", layoutKind: "HFlexLayout", flex: 1, components: [
 				{kind: "Spacer", flex: 1},
@@ -31,12 +78,14 @@ enyo.kind({
 		]}, 
 		{layoutKind: "VFlexLayout", flex: 1, components: [
 			{kind: "Divider", caption: "Keyboard"},
-			{layoutKind: "VFlexLayout", style: "padding: 5px 15px;", components: [
+			{layoutKind: "VFlexLayout", style: "padding: 5px 15px;margin-top: -5px;", components: [
 				{layoutKind: "HFlexLayout", style: "max-width: 320px; margin: auto auto;", components: [	
 					{name: "keyboardInput", kind: "ToolInput", alwaysLooksFocused: true, flex: 1, 
 						inputClassName: "keyboard-input", autoCapitalize: "lowercase", autoWordComplete: false, 
-						style: "margin: 0px 0px 0px 0px;", onkeypress: "handleKeypress", onfocus: "showKbdButtons",
-						onblur: "hideKbdButtons"},
+						style: "margin: 0px 0px 0px 0px;", onfocus: "showKbdButtons",
+						onblur: "hideKbdButtons", onkeydown: "handleKeyDown", onkeyup: "handleKeyUp"},
+					{name: "functionKey", caption: "FN", kind: "Button", className: "function-key enyo-button-dark", 
+						style: "margin-left: 10px;margin-right: 0px;", onclick: "handleFunctionKey"}
 				]}
 			]},
 			{kind: "Divider", caption: "Mouse", style: "margin-top: -5px;"},
@@ -86,6 +135,22 @@ enyo.kind({
 			onSuccess: "handleDeviceStatus"});	
 	},
 
+	handleFunctionKey: function() {
+		if(!this._function) {
+			this.$.functionKey.setDown(true);
+			
+			this._function = true;
+		
+			this.$.keysPopup.openAtCenter();
+		
+			this.$.keyboardInput.forceFocus();
+		} else {
+			this.$.functionKey.setDown(false);
+					
+			this._function = false;		
+		}
+	},
+
 	showKbdButtons: function() {
 		this.$.mouseLeft.hide();
 		this.$.mouseMiddle.hide();		
@@ -117,6 +182,15 @@ enyo.kind({
 	
 	cancelKbdBlur: function(inSender, inEvent) {
 		enyo.stopEvent(inEvent);
+	},
+	
+	handlePopupClose: function() {
+		if(this._function)
+			this.$.functionKey.setDown(true);
+
+		this.$.keysPopup.close();
+	
+		this.$.keyboardInput.forceFocus();
 	},
 	
 	handleButtonState: function(inSender, inEvent) {
@@ -181,15 +255,89 @@ enyo.kind({
 			onSuccess: "handleDeviceStatus"});
 	},
 
-	handleKeypress: function(inSender, inEvent) {
-		this.$.keyboardInput.setValue("");
+	handleKeyUp: function(inSender, inEvent) {
+		enyo.stopEvent(inEvent);
+		
+		var keys = {
+			"1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9", "0": "0",
+			"q": "q", "w": "w", "e": "e", "r": "r", "t": "t", "y": "y", "u": "u", "i": "i", "o": "o", "p": "p", "å": "å",
+			"a": "a", "s": "s",  "d": "d", "f": "f", "g": "g", "h": "h", "j": "j",  "k": "k", "l": "l", "ö": "ö", "ä": "ä",
+			"z": "z", "x": "x", "c": "c", "v": "v", "b": "b", "n": "n", "m": "m", 
+			"Q": "q", "W": "w", "E": "e", "R": "r", "T": "t", "Y": "y", "U": "u", "I": "i", "O": "o", "P": "p", "Å": "å",
+			"A": "a", "S": "s",  "D": "d", "F": "f", "G": "g", "H": "h", "J": "j",  "K": "k", "L": "l", "Ö": "ö", "Ä": "ä",
+			"Z": "z", "X": "x", "C": "c", "V": "v", "B": "b", "N": "n", "M": "m", 
+			"/": "slash", "+": "plus", "(": "parenleft", ")": "parenright", "%": "percent", "\"": "quotedbl", "=": "equal", 
+			"&": "ampersand", "-": "minus", "$": "dollar", "!": "exclam", ":": "colon", "'": "apostrophe", "*": "asterisk", 
+			"#": "numbersign", "?": "question", ";": "semicolon", "_": "underscore", ",": "comma", ".": "period", "at": "at",
+			"Backspace": "BackSpace", "Enter": "Return"
+		};
 
-		var action = "";
+		var fnKeys = {
+			"1": "F1", "2": "F2", "3": "F3", "4": "F4", "F5": "5", "6": "F6", "7": "F7", "8": "F8", "9": "F9", "0": "F10",
+			"e": "F1", "r": "F2", "t": "F3", "d": "F4", "f": "F5", "g": "F6", "x": "F7", "c": "F8", "v": "F9", "@": "F10",
+			"E": "F1", "R": "F2", "T": "F3", "D": "F4", "F": "F5", "G": "F6", "X": "F7", "C": "F8", "V": "F9", 
+			"q": "F11", "Q": "F11", "w": "F12", "W": "F12", "b": "Left", "B": "Left", "j": "Up", "J": "Up", "n": "Down", 
+			"N": "Down", "m": "Right", "M": "Right", "y": "Home", "Y": "Home", "h": "End", "H": "End", "u": "Insert",
+			"U": "Insert", "i": "Page_Up", "I": "Page_Up", "k": "Page_Down", "K": "Page_Down", " ": "Tab",
+			"Backspace": "Delete", "Enter": "Escape"
+		};
+		
+		if(inEvent.keyCode == 8)
+			var key = "Backspace";
+		else if(inEvent.keyCode == 13)
+			var key = "Enter";
+		else if(inEvent.keyCode == 48)
+			var key = "@";
+		else if(inEvent.keyCode == 49)
+			var key = "!";
+		else if(inEvent.keyCode == 52)
+			var key = "$";
+		else if(inEvent.keyCode == 53)
+			var key = "%";
+		else if(inEvent.keyCode == 66)
+			var key = "#";
+		else if(inEvent.keyCode == 90)
+			var key = "*";
+		else if(inEvent.keyCode == 222)
+			var key = "\"";
+		else
+			var key = String.fromCharCode(inEvent.keyCode);
 
-		action += String.fromCharCode(inEvent.keyCode);
+		if(((!this._function) && (keys[key])) || ((this._function) && (fnKeys[key]))) {
+			if(!this._function) {
+				if((this._shift) || (inEvent.keyCode == 8) || (inEvent.keyCode == 135))
+					this.$.keyboardInput.setValue(key);
+				else
+					this.$.keyboardInput.setValue(key.toLowerCase());
 
-		this.$.serverRequest.call({}, {url: "http://" + this.address + "/computer/keyboard?key=" + action, 
-			onSuccess: "handleDeviceStatus"});	
+				this.$.serverRequest.call({}, {url: "http://" + this.address + "/computer/keyboard?key=" + keys[key], 
+					onSuccess: "handleDeviceStatus"});	
+			} else {
+				this.$.keyboardInput.setValue(fnKeys[key].replace("_", " "));
+
+				this.$.serverRequest.call({}, {url: "http://" + this.address + "/computer/keyboard?key=" + fnKeys[key], 
+					onSuccess: "handleDeviceStatus"});	
+			}
+		} else if(inEvent.keyCode == 16) {
+			this._shift = false;
+		
+			this.$.serverRequest.call({}, {url: "http://" + this.address + "/computer/keyboard?up=Shift_L", 
+				onSuccess: "handleDeviceStatus"});		
+		} else if(inEvent.keyCode != 129) 
+			this.$.keyboardInput.setValue("<Unknown Key>");
+	},
+
+	handleKeyDown: function(inSender, inEvent) {
+		enyo.stopEvent(inEvent);
+		
+		if(inEvent.keyCode == 16) {
+			var key = "Shift";
+
+			this._shift = true;
+
+			this.$.serverRequest.call({}, {url: "http://" + this.address + "/computer/keyboard?down=Shift_L", 
+				onSuccess: "handleDeviceStatus"});		
+		}
 	},
 	
 	resetMouseEvent: function(inSender, inEvent) {	
